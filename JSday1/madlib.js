68cd560229ca8d512dc84349cf808d13ef79ba49
Aaron Smith <aaroncosmith@gmail.com>
2020-02-03 15:08:38 -0500

    added more work

---
+++ JSday1/madlib.js
@@ -0,0 +1,10 @@
+name1 = 'Aaron';
+class1 = 'Computers';
+
+function madlib(name, subject) {
+    return `${name}'s favorite subject in school is ${subject}`;
+}
+
+const dum = madlib(name1, class1);
+
+console.log(dum)
