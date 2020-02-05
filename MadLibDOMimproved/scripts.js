a665056fc35209d451863b6d32fbbd77681e03c2
Aaron Smith <aaroncosmith@gmail.com>
2020-02-05 13:30:02 -0500

    finshed madlib with proper js

---
+++ MadLibDOMimproved/scripts.js
@@ -0,0 +1,20 @@
+const allMadLib = document.querySelectorAll('.madlib');
+const getUserInputButton = document.querySelector('#clickMe');
+const userInputs = document.querySelectorAll(".userInput");
+    
+function parseUserInput(listOfInputs) {
+    const arrayOfValues = [];
+    listOfInputs.forEach(function(input){
+        arrayOfValues.push(input.value);
+    });
+    return arrayOfValues;
+}
+    
+getUserInputButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const userInputArray = parseUserInput(userInputs);
+
+    allMadLib.forEach(function(madlib, index){
+        madlib.innerHTML = userInputArray[index]
+    })
+});
