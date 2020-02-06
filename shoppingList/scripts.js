48a2a8176bddd0e6ab96dcf3bc42dfda241a4cc2
Aaron Smith <aaroncosmith@gmail.com>
2020-02-06 13:28:43 -0500

    finishing touches

--- shoppingList/scripts.js
+++ shoppingList/scripts.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const getUserInputButton = document.querySelector('#makeList');
+const userInputs = document.querySelectorAll(".userInput");
+const myUserList = document.querySelector('#userList')
+
+
+function makeMyList() {
+    userInputs.forEach(function(userInput){
+        const newText = document.createElement('p');
+        document.body.appendChild(newText)
+        newText.innerHTML += userInput.value;
+    });
+}
+
+
+
+getUserInputButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log("click");
+    console.log(userInputs);
+    makeMyList();
+});
+
+
